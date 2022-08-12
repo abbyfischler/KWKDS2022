3ede67434584c31cc93267099701bedfd7939a3a
Abby Fischler <abigail.fischler@gmail.com>
2022-08-12 08:26:03 -0700

    Added Day 4 Work!

---
+++ DAY4/day4notes.py
@@ -0,0 +1,5 @@
+# Intro to Google Colab
+# It’s a Jupyter notebook environment that requires no setup to use. In simpler terms, it’s a Jupyter notebook with all the collaboration abilities of Google docs, meaning more than one person can work on the same code at the same time. But, the real attraction is the free computing power that this tool offers.
+# To format text use markdown
+
+#Intro to Python
